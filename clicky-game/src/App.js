ac34a2b1806f0ed41b6bab308c5239111be07f1a
KevinWJohnson <kevin.johnson3@cox.net>
2018-08-13 21:15:08 -0700

    Number of curly brackets fixed in App.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,8 +5,7 @@ import Title from "./components/Title";
 import cartoons from "./cartoons.json";
 import "./App.css";
 
-
-var clickIdArray= [];
+var clickIdArray = [];
 
 function shuffleArray(array) {
   let i = array.length - 1;
@@ -19,7 +18,6 @@ function shuffleArray(array) {
   return array;
 }
 
-
 class App extends Component {
   // Setting this.state.cartoons to the cartoons json array
   state = {
@@ -29,60 +27,54 @@ class App extends Component {
     clickIdArray: []
   };
 
-  
   clickCartoon = id => {
-
     // Before the first cartoon is clicked the clickIdArray will be empty
     // array does not exist, is not an array, or is empty
     if (!Array.isArray(clickIdArray) || !clickIdArray.length) {
-
       // Adding 1 to the score
       this.setState({ score: this.state.score + 1 });
 
       // Adding id of clicked cartoon to array
-      const newIDArray = this.state.cartoons.map(cartoon => (
-      newIDArray.push(id)));
-      this.setState({clickIdArray:newIDArray})
-   
+      const newIDArray = this.state.cartoons.map(cartoon =>
+        newIDArray.push(id)
+      );
+      this.setState({ clickIdArray: newIDArray });
+
       // Shuffle the cartoons array
-      this.setState({cartoons:shuffleArray(cartoons)});
-      
+      this.setState({ cartoons: shuffleArray(cartoons) });
     } else {
-
       // Filter this.state.clickIdArray for cartoons with an id not equal to the id being clicked
-      const filclickIdArray = this.state.clickIdArray.filter(pickedID => pickedID.id === id);
-        
-    // The filclickIdArray will be empty is the cartoon was not clicked previously by the person
-    // array does not exist, is not an array, or is empty
-    if (!Array.isArray(filclickIdArray) || !filclickIdArray.length) {
-    
-       // Adding 1 to the score
-       this.setState({ score: this.state.score + 1 });
+      const filclickIdArray = this.state.clickIdArray.filter(
+        pickedID => pickedID.id === id
+      );
 
-       // Adding id of clicked cartoon to array
-       const newIDArray = this.state.cartoons.map(cartoon => (
-       newIDArray.push(id)));
-       this.setState({clickIdArray:newIDArray})
- 
-       // Shuffle the cartoons array
-      this.setState({cartoons:shuffleArray(cartoons)});
-      
-    } else {
+      // The filclickIdArray will be empty is the cartoon was not clicked previously by the person
+      // array does not exist, is not an array, or is empty
+      if (!Array.isArray(filclickIdArray) || !filclickIdArray.length) {
+        // Adding 1 to the score
+        this.setState({ score: this.state.score + 1 });
 
+        // Adding id of clicked cartoon to array
+        const newIDArray = this.state.cartoons.map(cartoon =>
+          newIDArray.push(id)
+        );
+        this.setState({ clickIdArray: newIDArray });
 
+        // Shuffle the cartoons array
+        this.setState({ cartoons: shuffleArray(cartoons) });
+      } else {
         // Updating the score
         this.setState({ score: 0 });
- 
-        // Clearing the ClickIdArray
-        this.setState({clickIdArray:[]});
- 
-         // Shuffle the cartoons array
-      this.setState({cartoons:shuffleArray(cartoons)});
 
-    };
+        // Clearing the ClickIdArray
+        this.setState({ clickIdArray: [] });
+
+        // Shuffle the cartoons array
+        this.setState({ cartoons: shuffleArray(cartoons) });
+      }
+    }
   };
 
-  
   // Map over this.state.cartoons and render a CartoonCard component for each cartoon object
   render() {
     return (
